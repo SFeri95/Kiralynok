ca6ba896c5af1894f9d19de509b1cd3e3d1dea22
SFeri95 <somiferi95@gmail.com>
2021-02-17 10:39:20 +0100

    OOP 6.feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -3,14 +3,33 @@ package kiralynok;
 import java.io.IOException;
 
 public class Kiralynok {
+    private Tabla tabla;
+    
+    public Kiralynok(){
+    tabla = new Tabla('#');
+    }
+    
     public static void main(String[] args) throws IOException {
-      Tabla tabla = new Tabla('#');
+        Kiralynok kir = new Kiralynok();
+        kir.megoldasok();
+    }
+        public void megoldasok(){
+        
+        }
+        private void fealadat4(){
+         Tabla tabla = new Tabla('#');
       System.out.println("4. feladat: Az üres tábla");
       tabla.megjelenit();
-      
-        System.out.println("6.feladat: A feltöltött tábla");
+        }
+     
+      private void fealadat6(){
+       System.out.println("6.feladat: A feltöltött tábla");
         tabla.elhelyez(8);
         tabla.megjelenit();
+      }
+      
+      
+       
     }
     
 }
