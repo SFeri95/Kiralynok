a0fdff4b2e3b0f6dbb2df7301eded4b831c15be5
SFeri95 <somiferi95@gmail.com>
2021-02-17 10:26:34 +0100

    4. Feladat Kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 public class Kiralynok {
     public static void main(String[] args) throws IOException {
-        
+      Tabla tabla = new Tabla('#');
+      System.out.println("4. feladat: Az üres tábla");
+      tabla.megjelenit();
     }
     
 }
